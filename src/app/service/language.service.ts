fa0a7b0537f78eb8c2d5ff7c3a12dd092c81fb16
EstebanM159 <estebanm91231@gmail.com>
2026-09-19 14:04:15 -0300

    Ajustar configuración de expiración y opciones de la cookie para el idioma

--- src/app/service/language.service.ts
+++ src/app/service/language.service.ts
@@ -1,13 +1,12 @@
 import { inject, InjectionToken, Service } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { SsrCookieService } from 'ngx-cookie-service-ssr';
-export const SERVER_LANG_TOKEN = new InjectionToken<string>('SERVER_LANG_TOKEN');
 @Service()
 export class LanguageService {
   cookie = inject(SsrCookieService);
   translate = inject(TranslateService);
   changeLanguage(lang: string) {
-    this.cookie.set('lang', lang);
+    this.cookie.set('lang', lang, { expires: 365, path: '/', sameSite: 'Lax' });
     this.translate.use(lang);
   }
 }
